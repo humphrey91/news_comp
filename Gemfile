831602c89cd9271269cbfe0d9a725aed41039f4e
Josh Humphrey <humphrey.js@gmail.com>
2017-05-23 16:31:15 +0700

    configure fog/aws

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'carrierwave'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'fog-aws'
+gem 'figaro'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,26 @@
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:  ENV["AWS_ACCESS_KEY"], # required
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],                        # required
+    region:                'us-west-2',                  # optional, defaults to 'us-east-1'
+    # host:                  's3.example.com',             # optional, defaults to nil
+    # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+  }
+  config.fog_directory  = 'newscomp'                          # required
+  config.fog_public     = false                                        # optional, defaults to true
+  config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" } # optional, defaults to {}
+end
+
+
+if Rails.env.test? or Rails.env.cucumber?
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.enable_processing = false
+  end
+else
+  CarrierWave.configure do |config|
+    config.storage = :fog
+  end
+end
