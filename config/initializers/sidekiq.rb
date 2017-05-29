5f7feca5e734bc43c8b022cfa80a084991a8327d
Josh Humphrey <humphrey.js@gmail.com>
2017-05-29 16:29:19 +0700

    try redis fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,23 @@
-Sidekiq.configure_server do |config|
-  schedule_file = "config/schedule.yml"
-
+def configure_cron
+  rails_root = File.dirname(__FILE__) + '/../..'
+  schedule_file = rails_root + "/config/schedule.yml"
   if File.exists?(schedule_file)
-    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+    sidekiq_cron = YAML.load_file(schedule_file)
+    Sidekiq::Cron::Job.load_from_hash sidekiq_cron[Rails.env]
   end
+end
+
+def configure_server(redis)
+  Sidekiq.configure_server do |config|
+    config.redis = { :url => redis, :namespace => 'sidekiq' }
+    configure_cron
+  end
+end
+
+if Rails.env == 'production'
+  # For production, run server configs
+  configure_server(redis)
+else
+  # For local, just start up the cron service
+  configure_cron
 end
