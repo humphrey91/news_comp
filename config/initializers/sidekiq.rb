ffcde51e3879a06f67577cbeacf40263c5615108
Josh Humphrey <humphrey.js@gmail.com>
2017-05-31 16:44:09 +0700

    fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,22 +5,12 @@ if Rails.env.production?
   end
 
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDIS_URL'], size: 22 }
-
-    Rails.application.config.after_initialize do
-
-      ActiveRecord::Base.connection_pool.disconnect!
-
-      ActiveSupport.on_load(:active_record) do
-        config = Rails.application.config.database_configuration[Rails.env]
-        config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
-        # config['pool'] = ENV['WORKER_DB_POOL_SIZE'] || Sidekiq.options[:concurrency]
-        config['pool'] = 16
-        ActiveRecord::Base.establish_connection(config)
-
-
-      end
-    end
+    config.redis = { url: ENV['REDIS_URL'] }
   end
 
+  schedule_file = "config/schedule.yml"
+
+  if File.exists?(schedule_file) && Sidekiq.server?
+    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+  end
 end
